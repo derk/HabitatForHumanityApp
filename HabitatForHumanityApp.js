5c87532dca53a196b2dd810890bb7a8467660aab
CarlSteven <carl_steven@live.com>
2015-06-08 08:18:35 -0500

    Made it build.

--- HabitatForHumanityApp.js
+++ HabitatForHumanityApp.js
@@ -1,18 +1,20 @@
-angular.module('inputIconDemo', ['ngMaterial', 'ngMessages'])
-  .controller('DemoCtrl', function($scope) {
-    $scope.user = {
-      name: '',
-      email: '',
-      password: ''
-    };
-  });
+if (Meteor.isClient) {
+    angular.module('inputIconDemo', ['ngMaterial', 'ngMessages'])
+      .controller('DemoCtrl', function($scope) {
+        $scope.user = {
+          name: '',
+          email: '',
+          password: ''
+        };
+      });
 
-angular.module('selectDemoValidation', ['ngMaterial', 'ngMessages'])
-.controller('AppCtrl', function($scope) {
-  $scope.clearValue = function() {
-    $scope.myModel = undefined;
-  };
-  $scope.save = function() {
-    alert('Form was valid!');
-  };
-});
+    angular.module('selectDemoValidation', ['ngMaterial', 'ngMessages'])
+    .controller('AppCtrl', function($scope) {
+      $scope.clearValue = function() {
+        $scope.myModel = undefined;
+      };
+      $scope.save = function() {
+        alert('Form was valid!');
+      };
+    });
+}
